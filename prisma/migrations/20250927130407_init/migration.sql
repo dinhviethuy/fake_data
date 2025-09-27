b76a06ed1e6e3cd7be69fb7502a34151b940218b
dinhviethuy <dinhviethuy055@gmail.com>
2025-09-27 20:05:43 +0700

    fake data

--- prisma/migrations/20250927130407_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `level` on the `job` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `job` DROP COLUMN `level`;
